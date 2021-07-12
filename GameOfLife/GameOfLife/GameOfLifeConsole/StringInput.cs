f3ecffcb529349b7a4842198df409dbad777a248
Leah Filkin <leah.filkin@myob.com>
2021-07-12 16:15:51 +1200

    split up validation steps into seperate methods

--- GameOfLife/GameOfLife/GameOfLifeConsole/StringInput.cs
+++ GameOfLife/GameOfLife/GameOfLifeConsole/StringInput.cs
@@ -27,20 +27,33 @@ namespace GameOfLife.GameOfLifeConsole
 
         public static void Validate(List<string> input)
         {
-            if (input.Any(row => row.Any(character => char.ToLower(character) != 'x' && character != '.')))
+            CheckOnlyPeriodsAndXCharactersAreUsed(input);
+            CheckAllRowsHaveSameAmountOfColumns(input);
+            CheckThereIsAtLeastOneActiveCellToBeginWith(input);
+        }
+
+        private static void CheckThereIsAtLeastOneActiveCellToBeginWith(List<string> input)
+        {
+            if (input.All(row => row.All(character => char.ToLower(character) != 'x')))
             {
-                throw new ArgumentException("You can only use x and . characters in your input");
+                throw new ArgumentException("Your grid should have at least one live cell for the initial state");
             }
+        }
+
+        private static void CheckAllRowsHaveSameAmountOfColumns(List<string> input)
+        {
             if (input.Any(row => row.Length != input[0].Length))
             {
                 throw new ArgumentException("Your grid should have the same width for all rows");
             }
+        }
 
-            if (input.All(row => row.All(character => char.ToLower(character) != 'x')))
+        private static void CheckOnlyPeriodsAndXCharactersAreUsed(List<string> input)
+        {
+            if (input.Any(row => row.Any(character => char.ToLower(character) != 'x' && character != '.')))
             {
-                throw new ArgumentException("Your grid should have at least one live cell for the initial state");
-
-            }
+                throw new ArgumentException("You can only use x and . characters in your input");
+            }        
         }
 
         public static Grid ConvertToGrid(string path)
